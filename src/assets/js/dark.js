4bcd91c382d933844d787951f7f1af88bafdcb6a
Paul Tucker <ptucker@thinkrite.com>
2023-06-18 23:57:53 -0400

    always dark mode

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -27,14 +27,15 @@ function detectColorScheme() {
 	}
 
 	// if there is no preference set, the default of light will be used. apply accordingly
-	theme === 'dark' ? enableDarkMode() : disableDarkMode();
+	// theme === 'dark' ? enableDarkMode() : disableDarkMode( );
+	enableDarkMode();
 }
 
 // run on page load
 detectColorScheme();
 
 // add event listener to the dark mode button toggle
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
-	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
-});
+// document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+// 	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
+// 	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
+// });
